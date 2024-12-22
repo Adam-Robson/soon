7b4733568474aa47b397f314d1636a65766bc419
Adam Robson <adamrayrobson@gmail.com>
2024-12-22 00:01:40 -0800

    add content

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="karla">You've reached the home page for Le Fog.</h1>
+      <h2 className="karla">Le Fog is a band from Portland, Oregon.</h2>
+      <img src="pie.svg" alt="le fog bg" className="bg" />
+      <h3 className="karla">Check back soon!</h3>
     </>
-  )
+  );
 }
-
-export default App
